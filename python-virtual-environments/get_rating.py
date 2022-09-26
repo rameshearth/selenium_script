1cb6f99a32e4256c952c1594589cfa22797dfe19
ramesh.makos <ramesh.makos@gmail.com>
2022-09-26 14:36:04 +0530

    implement exception handling

--- python-virtual-environments/get_rating.py
+++ python-virtual-environments/get_rating.py
@@ -7,31 +7,60 @@ from requests_toolbelt.multipart.encoder import MultipartEncoder
 import os
 
 
-url = 'http://localhost/Xplore-new-22-12-21/xplore/index.php/apis/getPlaceDetail';
+#Local APIS links
+fetch_places_api = 'http://localhost/Xplore-new-22-12-21/xplore/index.php/apis/getPlaceDetail';
+update_place_data_api = 'http://localhost/Xplore-new-22-12-21/xplore/index.php/apis/insertPlaceDetail';
 
-response = ur.urlopen(url)
+#Live APIS Links
+#fetch_places_api = 'https://xplore-now.co/index.php/apis/getPlaceDetail';
+#update_place_data_api = 'https://xplore-now.co/index.php/apis/insertPlaceDetail';
+
+response = ur.urlopen(fetch_places_api)
 PlaceDetail = json.loads(response.read())
 
-
+# 1) Trip Advisor configration
 DRIVER_PATH1='/home/this/Downloads/web_selenium/python-virtual-environments/chromedriver105_1'
-DRIVER_PATH2 = '/home/this/Downloads/web_selenium/python-virtual-environments/chromedriver105_2'
-DRIVER_PATH3 = '/home/this/Downloads/web_selenium/python-virtual-environments/chromedriver105_3'
-DRIVER_PATH4 = '/home/this/Downloads/web_selenium/python-virtual-environments/chromedriver105_4'
-DRIVER_PATH5 = '/home/this/Downloads/web_selenium/python-virtual-environments/chromedriver105_5'
-
 driver1=webdriver.Chrome(executable_path=DRIVER_PATH1)
+trip_rating_xpath = '/html/body/div[2]/div[2]/div[2]/div[2]/div/div[1]/div/div[1]/div/div[1]/div[1]/span[1]'
+trip_review_xpath = '/html/body/div[2]/div[2]/div[2]/div[2]/div/div[1]/div/div[1]/div/div[1]/div[1]/a'
+
+
+# 2) Open Table  configration
+DRIVER_PATH2 = '/home/this/Downloads/web_selenium/python-virtual-environments/chromedriver105_2'
 driver2 = webdriver.Chrome(executable_path=DRIVER_PATH2)
+open_rating_xpath = '/html/body/div[1]/div/div/main/div/div[2]/div[1]/section[2]/div[1]/div[1]/span'
+open_review_xpath = '/html/body/div[1]/div/div/main/div/div[2]/div[1]/section[2]/div[1]/div[2]/span[2]'
+
+# 3) Google configration 
+DRIVER_PATH3 = '/home/this/Downloads/web_selenium/python-virtual-environments/chromedriver105_3'
 driver3 = webdriver.Chrome(executable_path=DRIVER_PATH3)
+google_rating_xpath = '/html/body/div[7]/div/div[11]/div[2]/div/div/div[2]/div/div[1]/div[2]/div[2]/div/div/span[1]'
+google_review_xpath = '/html/body/div[7]/div/div[11]/div[2]/div/div/div[2]/div/div[1]/div[2]/div[2]/div/div/span[3]/span/a/span'
+
+#4) Door Dash configration 
+DRIVER_PATH4 = '/home/this/Downloads/web_selenium/python-virtual-environments/chromedriver105_4'
 driver4 = webdriver.Chrome(executable_path=DRIVER_PATH4)
+door_rating_xpath = '/html/body/div[1]/main/div/div[1]/div[1]/div/header/div[2]/div[1]/div[1]/div/div[3]/div/span[1]'
+door_review_xpath = '/html/body/div[1]/main/div/div[1]/div[1]/div/header/div[2]/div[1]/div[1]/div/div[3]/div/span[2]'
+
+# Yelp  configration
+DRIVER_PATH5 = '/home/this/Downloads/web_selenium/python-virtual-environments/chromedriver105_5'
 driver5 = webdriver.Chrome(executable_path=DRIVER_PATH5)
+yelp_rating_xpath = '/html/body/yelp-react-root/div[1]/div[3]/div[1]/div[1]/div/div/div[2]/div[1]/span/div'
+yelp_review_xpath = '/html/body/yelp-react-root/div[1]/div[3]/div[1]/div[1]/div/div/div[2]/div[2]/span'
+
+
 
 os.system('cls||clear')
 print("********************************************** ")
+
+whitelist = set('1234567890.,')
 final_array=[]
-# 1) Trip Advisor 2) Open Table 3) Google 4) Door Dash 5) Yelp 
-# ((number of ratings / total ratings) * rating)) 
+
+# ((number of review / total review) * rating)) 
 
 for place in PlaceDetail:
+	print('SrNumber',place['SrNumber']);
 	overall_rating=0
 	trip_advisor_url=place['trip_advisor_url']
 	trip_rating=0
@@ -50,65 +79,88 @@ for place in PlaceDetail:
 	yelp_review=0
 	total_reviews=0;
 
-	whitelist = set('1234567890.')
-
 	#############
 	############# Trip advisor website rating
 	if trip_advisor_url is not None:
-		driver1.get(trip_advisor_url)
-		rating=driver1.find_element('xpath','/html/body/div[2]/div[2]/div[2]/div[2]/div/div[1]/div/div[1]/div/div[1]/div[1]/span[1]')
-		trip_rating = ''.join(filter(whitelist.__contains__,rating.text))
-		print("trip_advisor Rating : ",trip_rating)
-		reviews=driver1.find_element('xpath','/html/body/div[2]/div[2]/div[2]/div[2]/div/div[1]/div/div[1]/div/div[1]/div[1]/a')
-		trip_review = ''.join(filter(whitelist.__contains__,reviews.text))
-		print("trip_advisor Review : ",trip_review)
-		total_reviews=total_reviews +int(trip_review);
-	########## open table rating
+		try:
+			driver1.get(trip_advisor_url)
+			rating=driver1.find_element('xpath',trip_rating_xpath)
+			trip_rating = ''.join(filter(whitelist.__contains__,rating.text)).replace(",", "")
+			print("trip_advisor Rating : ",trip_rating)
+			reviews=driver1.find_element('xpath',trip_review_xpath)
+			trip_review = ''.join(filter(whitelist.__contains__,reviews.text)).replace(",", "")
+			print("trip_advisor Review : ",trip_review)
+			total_reviews=total_reviews +float(trip_review);
+		except Exception as e:
+			if 'particular message' in str(e):
+				continue
+			########## open table rating
 	if open_table_url is not None:
-		driver2.get(open_table_url)
-		open_table_rating=driver2.find_element('xpath','/html/body/div[1]/div/div/main/div/div[2]/div[1]/section[2]/div[1]/div[1]/span')
-		open_rating = ''.join(filter(whitelist.__contains__,open_table_rating.text))
-		print("Open table Rating : ",open_rating)
-		reviews=driver2.find_element('xpath','/html/body/div[1]/div/div/main/div/div[2]/div[1]/section[2]/div[1]/div[2]/span[2]')
-		open_review = ''.join(filter(whitelist.__contains__,reviews.text))
-		print("Open table reviews : ",open_review)
-		total_reviews=total_reviews +int(open_review);
+		try:
+			driver2.get(open_table_url)
+			open_table_rating=driver2.find_element('xpath',open_rating_xpath)
+			open_rating = ''.join(filter(whitelist.__contains__,open_table_rating.text)).replace(",", "")
+			print("Open table Rating : ",open_rating)
+			reviews=driver2.find_element('xpath',open_review_xpath)
+			open_review = ''.join(filter(whitelist.__contains__,reviews.text)).replace(",", "")
+			print("Open table reviews : ",open_review)
+			total_reviews=total_reviews +float(open_review);
+		except Exception as e:
+			if 'particular message' in str(e):
+				continue
 	####
 	###################### door
 	if google_url is not None:
-		driver3.get(google_url)
-		google_rating1=driver3.find_element('xpath','/html/body/div[7]/div/div[11]/div[2]/div/div/div[2]/div/div[1]/div[2]/div[2]/div/div/span[1]')
-		google_rating=''.join(filter(whitelist.__contains__,google_rating1.text))
-		print("Google Rating : ",google_rating)
-		reviews=driver3.find_element('xpath','/html/body/div[7]/div/div[11]/div[2]/div/div/div[2]/div/div[1]/div[2]/div[2]/div/div/span[3]/span/a/span')
-		google_review=''.join(filter(whitelist.__contains__,reviews.text))
-		print("Google reviews : ",google_review)
-		total_reviews=total_reviews +int(google_review);
-	######################
+		try:
+			driver3.get(google_url)
+			google_rating1=driver3.find_element('xpath',google_rating_xpath)
+			google_rating=''.join(filter(whitelist.__contains__,google_rating1.text)).replace(",", "")
+			print("Google Rating : ",google_rating)
+			reviews=driver3.find_element('xpath',google_review_xpath)
+			google_review=''.join(filter(whitelist.__contains__,reviews.text)).replace(",", "")
+			print("Google reviews : ",google_review)
+			total_reviews=total_reviews +float(google_review);
+		except Exception as e:
+			if 'particular message' in str(e):
+				continue
+			######################
 	if door_dash_url is not None:
-		driver4.get(door_dash_url)
-		doordash_rating=driver4.find_element('xpath','/html/body/div[1]/main/div/div[1]/div[1]/div/header/div[2]/div[1]/div[1]/div/div[3]/div/span[1]')
-		door_rating = ''.join(filter(whitelist.__contains__,doordash_rating.text))
-		print("Door Dash Rating : ",door_rating)
-		reviews=driver4.find_element('xpath','/html/body/div[1]/main/div/div[1]/div[1]/div/header/div[2]/div[1]/div[1]/div/div[3]/div/span[2]')
-		door_review = ''.join(filter(whitelist.__contains__,reviews.text))
-		print("Door Dash reviews : ",door_review)
-		total_reviews=total_reviews +int(door_review);
+		try:
+			driver4.get(door_dash_url)
+			doordash_rating=driver4.find_element('xpath',door_rating_xpath)
+			door_rating = ''.join(filter(whitelist.__contains__,doordash_rating.text)).replace(",", "")
+			print("Door Dash Rating : ",door_rating)
+			reviews=driver4.find_element('xpath',door_review_xpath)
+			door_review = ''.join(filter(whitelist.__contains__,reviews.text)).replace(",", "")
+			print("Door Dash reviews : ",door_review)
+			total_reviews=total_reviews +float(door_review);
+		except Exception as e:
+			if 'particular message' in str(e):
+				continue
+
 	if yelp_url is not None:
-		driver5.get(yelp_url)
-		yelp_url_rating=driver5.find_element('xpath','/html/body/yelp-react-root/div[1]/div[3]/div[1]/div[1]/div/div/div[2]/div[1]/span/div')
-		text = yelp_url_rating.get_attribute("aria-label")
-		answer = ''.join(filter(whitelist.__contains__, text))
-		yelp_rating = answer
-		print("Yelp Rating : ",yelp_rating)
-		reviews=driver5.find_element('xpath','/html/body/yelp-react-root/div[1]/div[3]/div[1]/div[1]/div/div/div[2]/div[2]/span')
-		yelp_review = ''.join(filter(whitelist.__contains__,reviews.text))
-		print("Yelp Reviews : ",yelp_review)
-		total_reviews=total_reviews +int(yelp_review);
-		print("total_reviews :",total_reviews )
-		#overall_rating = float(trip_rating)+float(open_rating)+float(door_rating)+float(yelp_rating)
-		#overall_rating = (float(trip_rating)+float(open_rating)+float(google_rating)+float(yelp_rating)+float(yelp_rating))/5
+		try:
+			driver5.get(yelp_url)
+			yelp_url_rating=driver5.find_element('xpath',yelp_rating_xpath)
+			text = yelp_url_rating.get_attribute("aria-label")
+			answer = ''.join(filter(whitelist.__contains__, text)).replace(",", "")
+			yelp_rating = answer
+			print("Yelp Rating : ",yelp_rating)
+			reviews=driver5.find_element('xpath',yelp_review_xpath)
+			yelp_review = ''.join(filter(whitelist.__contains__,reviews.text)).replace(",", "")
+			print("Yelp Reviews : ",yelp_review)
+			total_reviews=total_reviews +float(yelp_review);
+		except Exception as e:
+			if 'particular message' in str(e):
+				continue
+	print("total_reviews :",total_reviews )
+	#overall_rating = float(trip_rating)+float(open_rating)+float(door_rating)+float(yelp_rating)
+	#overall_rating = (float(trip_rating)+float(open_rating)+float(google_rating)+float(yelp_rating)+float(yelp_rating))/5
+	if(total_reviews == 0):
+		overall_rating = 0
+	else:
 		overall_rating = ((float(trip_review)/total_reviews)*float(trip_rating)) + ((float(open_review)/total_reviews)*float(open_rating)) + ((float(google_review)/total_reviews)*float(google_rating)) + ((float(door_review)/total_reviews)*float(door_rating)) + ((float(yelp_review)/total_reviews)*float(yelp_rating))
+
 	final_array.append({"SrNumber": place['SrNumber'], "overall_rating": overall_rating});
 
 
@@ -118,7 +170,7 @@ print(params)
 data = MultipartEncoder(fields = params)
 headers = {'Content-type': data.content_type}
 response = requests.post(
-    'http://localhost/Xplore-new-22-12-21/xplore/index.php/apis/insertPlaceDetail',
+    update_place_data_api,
     data = data,
     headers = headers
 )
@@ -132,8 +184,6 @@ print(response.text)
 #print(url.json());
 
 """
-
-
 driver.get("https://www.tripadvisor.com/Restaurant_Review-g35805-d10066453-Reviews-Immm_Rice_Beyond-Chicago_Illinois.html")
 
 
--- python-virtual-environments/simple.py
+++ python-virtual-environments/simple.py
@@ -1,14 +1,35 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.select import Select
-import time 
+import time
+import urllib.request as ur
+import requests
+import json
+from requests_toolbelt.multipart.encoder import MultipartEncoder
+import os
 
-DRIVER_PATH = '/home/this/Downloads/web_selenium/python-virtual-environments/chromedriver105'
 
-driver = webdriver.Chrome(executable_path=DRIVER_PATH)
-driver.get("https://www.tripadvisor.com")
+DRIVER_PATH = '/home/this/Downloads/web_selenium/python-virtual-environments/chromedriver105_1'
+driver4 = webdriver.Chrome(executable_path=DRIVER_PATH)
+whitelist = set(',1234567890.,')
+total_reviews=0
+door_dash_url="https://www.doordash.com/store/little-v-vietnamese-bistro-(fm-1463-rd)-katy-24002/"
+door_rating_xpath = '/html/body/div[1]/main/div/div[1]/div[1]/div/header/div[2]/div[1]/div[1]/div/div[3]/div/span[1]'
+door_review_xpath = '/html/body/div[1]/main/div/div[1]/div[1]/div/header/div[2]/div[1]/div[1]/div/div[3]/div/span[2]'
+driver4.get(door_dash_url)
+doordash_rating=driver4.find_element('xpath',door_rating_xpath)
+door_rating = ''.join(filter(whitelist.__contains__,doordash_rating.text)).replace(",", "")
+print("Door Dash Rating : ",door_rating)
+reviews=driver4.find_element('xpath',door_review_xpath)
+door_review = ''.join(filter(whitelist.__contains__,reviews.text)).replace(",", "")
+print("Door Dash reviews : ",door_review)
+total_reviews=total_reviews +float(door_review);
+#open_review = ''.join(filter(whitelist.__contains__,reviews.text))
+#print(open_review);
+print("door_review",door_review);
+print("door_rating",door_rating);
 
-
+"""
 driver.find_element("link text","Restaurants").click();
 time.sleep(3)
 driver.find_element("xpath","/html/body/div[2]/div/form/input[1]").send_keys("Immm Rice & Beyond");
@@ -23,3 +44,4 @@ time.sleep(3)
 #//*[@id="component_50"]/div[1]/div/div[1]/div/div[1]/div[1]/span[1]
 
 #/html/body/div[2]/div[2]/div[2]/div[2]/div/div[1]/div/div[1]/div/div[1]/div[1]/span[1]
+"""
